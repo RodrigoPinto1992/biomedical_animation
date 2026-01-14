be5d55dfd17ce48a64064f50bde06c9e03fcb60c
Rodrigo Pinto <rodrigo.pinto@udb.edu.sv>
2026-01-14 11:17:27 -0600

    actualizacion de animacion p01_autorefractometro: funcionalidad ccompleta

--- p01_autorefractometro/js/botones.js
+++ p01_autorefractometro/js/botones.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const luz = document.getElementById('luz');
+    const luz_emetrope = document.getElementById('luz-emetrope');
+    const luz_miope = document.getElementById('luz-miope');
+    const luz_hipermetrope = document.getElementById('luz-hipermetrope');
     const botonAnimar = document.getElementById('btnAnimar');
     const botonBorar = document.getElementById('btnBorrar');
 
     botonAnimar.addEventListener('click', () => {
         //Añadir la clase 'animada' para iniciar la animación
-        luz.classList.add('animada');
+        luz_emetrope.classList.add('animada');
+        luz_miope.classList.add('animada');
+        luz_hipermetrope.classList.add('animada');
     });
 
     botonBorar.addEventListener('click', () => {
-        luz.classList.remove('animada');
+        luz_emetrope.classList.remove('animada');
+        luz_miope.classList.remove('animada');
+        luz_hipermetrope.classList.remove('animada');
     });
 });
